5ac92ee92c22ea191af2d3963854ffa47e604991
adamdenzil <80000581+adamdenzil@users.noreply.github.com>
2021-12-02 18:08:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+song="";
+
+scoreleftwrist=0;
+scorerightwrist=0;
+
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rightwristy=0;
+
+function setup(){
+canvas=createCanvas(600,500);
+canvas.center();
+
+video=createCapture(VIDEO);
+video.hide();
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results){
+if (results.length>0) {
+console.log(results);
+
+scoreleftwrist=results[0].pose.keypoints[9].score;
+scorerightwrist=results[0].pose.keypoints[10].score;
+
+leftwristx=results[0].pose.leftWrist.x;
+leftwristy=results[0].pose.leftWrist.y;
+
+rightwristx=results[0].pose.rightWrist.x;
+rightwristy=results[0].pose.rightWrist.y;
+}    
+}
+
+function modelLoaded(){
+console.log('poseNet worked yay :)');    
+}
+
+function draw(){
+image(video,0,0,600,500);
+
+fill("#FF0000");
+stroke("#FF0000");
+
+if (scoreleftwrist>0.2) {
+    circle(leftwristx,leftwristy,20);
+    InNumberleftwristY=Number(leftwristy);
+    remove_decimals=floor(InNumberleftwristY);
+    volume=remove_decimals/500;
+    document.getElementById("volume").innerHTML="volume= "+volume;
+    song.setVolume(volume);    
+}
+
+if (scorerightwrist>0.2) {
+    circle(rightwristx,rightwristy,20);
+ 
+    if (rightwristy>0 && rightwristy<=100) {
+    document.getElementById("speed").innerHTML="speed=0.5x";
+    song.rate(0.5);    
+    }
+    else  if (rightwristy>100 && rightwristy<=200) {
+        document.getElementById("speed").innerHTML="speed=1x";
+        song.rate(1);    
+}
+else if (rightwristy>200 && rightwristy<=300) {
+    document.getElementById("speed").innerHTML="speed=1.5x";
+    song.rate(1.5);
+}
+else if (rightwristy>300 && rightwristy<=400) {
+    document.getElementById("speed").innerHTML="speed=2x";
+    song.rate(2);
+}
+else if (rightwristy>400 ) {
+    document.getElementById("speed").innerHTML="speed=2.5x";
+    song.rate(2.5);
+}
+}
+}
+
+function preload(){
+song=loadSound("music.mp3");    
+}
+
+function play(){
+song.play();
+song.rate(1);
+song.setVolume(1);
+}
